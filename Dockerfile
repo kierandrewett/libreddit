5ef2298076be36016d1c45691bb7b1176e3bd337
EnderDev <kieran@dothq.co>
2020-12-23 17:41:06 +0000

    Initial Commit

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM rust:alpine as builder
-WORKDIR /usr/src/libreddit
-COPY . .
-RUN apk add --no-cache g++ openssl-dev
-RUN cargo install --path .
-
-FROM alpine:latest
-COPY --from=builder /usr/local/cargo/bin/libreddit /usr/local/bin/libreddit
-CMD ["libreddit"]
---
+++ index.js
@@ -0,0 +1,21 @@
+const app = require('express')()
+const child_process = require("child_process"); 
+var requestProxy = require("express-request-proxy");
+
+child_process.spawn("./libreddit", { stdio: "inherit", cwd: process.cwd() }).on("exit", () => {
+    process.exit(-1);
+})
+
+app.use((req, res, next) => {
+    var p = requestProxy({
+        url: `http://localhost:8080${req.path}`,
+    })
+
+    p(req, res, next);
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port)
+
+module.exports = app
